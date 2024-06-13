6d37ece5a033b14189a67baeaefcc105b51d7f9b
냐부기 <68580993+dorameme@users.noreply.github.com>
2024-06-13 10:12:43 +0900

    Create palindrome-linked-list.java

---
+++ palindrome-linked-list.java
@@ -0,0 +1,40 @@
+/*
+공부한 날짜: 2024-06-13
+시간복잡도:O(n)
+공간복잡도:O(n)
+*/
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while(true){
+            if(head==null){break;}
+            list.add(head.val);
+            head=head.next;
+        }
+
+        List<Integer> list1= new ArrayList<>();
+        List<Integer> list2= new ArrayList<>();
+        if(list.size()%2==0){
+            list1 = list.subList(0, list.size() / 2);
+            list2 = list.subList(list.size() / 2, list.size());
+        }else{
+            list1 = list.subList(0, list.size() / 2);
+            list2 = list.subList(list.size() / 2+1, list.size());
+        }
+        return list1.equals(list2.reversed());
+    }
+}
