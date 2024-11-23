8cca6583abee583cdd3abdf91b9c4bcc8260a576
냐부기 <68580993+dorameme@users.noreply.github.com>
2024-11-23 14:09:48 +0900

    Update BOJ14651걷다보니 신천역 삼 (Large)(silver1).java

--- 다이나믹프로그래밍/BOJ14651걷다보니 신천역 삼 (Large)(silver1).java
+++ 다이나믹프로그래밍/BOJ14651걷다보니 신천역 삼 (Large)(silver1).java
@@ -4,10 +4,45 @@
  * 시간복잡도:O(N)
  * 공간복잡도:O(N)
  * 풀이: 합이 0으로 끝나는 경우 2로 끝나는경우 3으로 끝나는 경우를 구해준다.
- 근데 사실 모든 dp[i] [0]~[2] 다같으니까 dp안풀어도 되긴함 ㅋㅋ
+ 근데 사실 모든 dp[i] [0]~[2] 다같으니까 dp안풀어도 되긴함 ㅋㅋ 근데 주의할게 MOD로 꼭 답을 나눠줘야함 그거안해서 처음에 틀림 --
+
+ * */
+package  배열;/*
+ * start:21:16
+ * end:
+ * 시간복잡도:
+ * 공간복잡도:
+ * 풀이:
 
  * */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static final int MOD=1000000009;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if(N==1){
+            System.out.println(0);return;}
+        long answer=1;
+        for(int i=2;i<N;i++){
+            answer*=3;
+            answer%=MOD;
+        }
+        System.out.println(answer*2%MOD);///이부분 중요!!
+    }
+}
+
+//dp 풀이
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
